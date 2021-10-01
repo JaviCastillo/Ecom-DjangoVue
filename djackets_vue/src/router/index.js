1cfe15c962c20119b3bce07ae057978391a2a530
Javier Castillo <jav.castonatt@gmail.com>
2021-10-01 14:56:07 -0300

    account and checkout

--- djackets_vue/src/router/index.js
+++ djackets_vue/src/router/index.js
@@ -1,4 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+import store from '../store'
+
 import Home from '../views/Home.vue'
 
 import Product from '../views/Product.vue'
@@ -7,6 +10,8 @@ import Search from '../views/Search.vue'
 import Cart from '../views/Cart.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
+import MyAccount from '../views/MyAccount.vue'
+import Checkout from '../views/Checkout.vue'
 
 const routes = [
   {
@@ -23,6 +28,24 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/log-in',
+    name: 'LogIn',
+    component: LogIn
+  },
+  {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requiredLogin: true
+    }
+  },
+  {
     path: '/search',
     name: 'Search',
     component: Search
@@ -33,14 +56,12 @@ const routes = [
     component: Cart
   },
   {
-    path: '/sign-up',
-    name: 'SignUp',
-    component: SignUp
-  },
-  {
-    path: '/log-in',
-    name: 'LogIn',
-    component: LogIn
+    path: '/cart/checkout',
+    name: 'Checkout',
+    component: Checkout,
+    meta: {
+      requiredLogin: true
+    }
   },
   {
     path: '/:category_slug/:product_slug/',
@@ -57,6 +78,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({ name: 'LogIn', query: { to: to.path } })
+  } else {
+    next()
+  }
 })
 
 export default router
